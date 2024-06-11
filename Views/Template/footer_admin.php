01f1f5cb34b30a24bb17322a34f48b1d9053a5cc
ARIANNAEF22 <2101010123@undc.edu.pe>
2024-06-11 01:20:00 -0500

    Nuevo diseño del footer

--- Views/Template/footer_admin.php
+++ Views/Template/footer_admin.php
@@ -1,38 +1,98 @@
-    <script>
-        const base_url = "<?= base_url(); ?>";
-        const smony = "<?= SMONEY; ?>";
-    </script>
-    <!-- Essential javascripts for application to work-->
-    <script src="<?= media(); ?>/js/jquery-3.3.1.min.js"></script>
+<?php 
+		$catFotter = getCatFooter();
+	 ?>
+	<!-- Footer -->
+	<footer class="bg3 p-t-75 p-b-32">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6 col-lg-4 p-b-50">
+					<h4 class="stext-301 cl0 p-b-30">
+						Categorías
+					</h4>
+					<?php if(count($catFotter) > 0){ ?>
+					<ul>
+						<?php foreach ($catFotter as $cat) { ?>
+						<li class="p-b-10">
+							<a href="<?= base_url() ?>/tienda/categoria/<?= $cat['idcategoria'].'/'.$cat['ruta'] ?>" class="stext-107 cl7 hov-cl1 trans-04">
+								<?= $cat['nombre'] ?>
+							</a>
+						</li>
+						<?php } ?>
+					</ul>
+					<?php } ?>
+				</div>
 
-    <script src="<?= media(); ?>/js/popper.min.js"></script>
-    <script src="<?= media(); ?>/js/bootstrap.min.js"></script>
-    <script src="<?= media(); ?>/js/main.js"></script>
-    <script src="<?= media();?>/js/fontawesome.js"></script>
-    <!-- The javascript plugin to display page loading on top-->
-    <script src="<?= media(); ?>/js/plugins/pace.min.js"></script>
-    <!-- Page specific javascripts-->
-    <script type="text/javascript" src="<?= media(); ?>/js/plugins/sweetalert.min.js"></script>
-    <script type="text/javascript" src="<?= media(); ?>/js/tinymce/tinymce.min.js"></script>
+				<div class="col-sm-6 col-lg-4 p-b-50">
+					<h4 class="stext-301 cl0 p-b-30">
+						Contacto
+					</h4>
 
-    <!-- Data table plugin-->
-    <script type="text/javascript" src="<?= media(); ?>/js/plugins/jquery.dataTables.min.js"></script>
-    <script type="text/javascript" src="<?= media(); ?>/js/plugins/dataTables.bootstrap.min.js"></script>
-    <script type="text/javascript" src="<?= media();?>/js/plugins/bootstrap-select.min.js"></script>
-    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.5.2/js/dataTables.buttons.min.js"></script>
-    <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/pdfmake.min.js"></script>
-    <script type="text/javascript" language="javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.36/vfs_fonts.js"></script>
-    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/buttons/1.5.2/js/buttons.html5.min.js"></script>
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <script src="https://code.highcharts.com/modules/exporting.js"></script>
-    <script src="https://code.highcharts.com/modules/export-data.js"></script>
-    <script src="<?= media();?>/js/datepicker/jquery-ui.min.js"></script>
-    <script type="text/javascript" src="<?= media();?>/js/functions_admin.js"></script>
-    <script src="<?= media(); ?>/js/<?= $data['page_functions_js']; ?>"></script>
+					<p class="stext-107 cl7 size-201">
+						<?= DIRECCION ?> <br>
+						Celular: <a class="linkFooter" href="tel:<?= TELEMPRESA ?>"><?= TELEMPRESA ?></a><br>
+						Correo pedidos: <a class="linkFooter" href="mailto:<?= EMAIL_PEDIDOS ?>"><?= EMAIL_PEDIDOS ?></a><br>
+						Correo dueña: <a class="linkFooter" href="mailto:<?= EMAIL_EMPRESA ?>"><?= EMAIL_EMPRESA ?></a>
+					</p>
 
-    <script src="<?= media(); ?>/js/functionsCotizaciones.js"></script>
-    <script src="<?= media(); ?>/js/functionsCotizar.js"></script>
-    
-  </body>
+					<div class="p-t-27">
+						<a href="<?= FACEBOOK ?>" target="_blank" class="fs-18 cl7 hov-cl1 trans-04 m-r-16">
+							<i class="fa fa-facebook"></i>
+						</a>
+
+						<a href="https://wa.me/<?= WHATSAPP ?>" target="_blank" class="fs-18 cl7 hov-cl1 trans-04 m-r-16">
+							<i class="fab fa-whatsapp"></i>
+						</a>
+					</div>
+				</div>
+
+				<div class="col-sm-12 col-lg-4 p-b-50 d-flex align-items-center">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d242.91223849702723!2d-76.35255016531241!3d-13.061379830337268!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x910ffbdd4c8cdadf%3A0x80e25b1cfd73cd6e!2sRicky%20Pan!5e0!3m2!1ses!2spe!4v1717949702216!5m2!1ses!2spe" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	<!-- Back to top -->
+	<div class="btn-back-to-top" id="myBtn">
+		<span class="symbol-btn-back-to-top">
+			<i class="zmdi zmdi-chevron-up"></i>
+		</span>
+	</div>
+	<script>
+	    const base_url = "<?= base_url(); ?>";
+		const smony = "<?= SMONEY; ?>";
+	</script>
+<!--===============================================================================================-->	
+	<script src="<?= media() ?>/tienda/vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= media() ?>/tienda/vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= media() ?>/tienda/vendor/bootstrap/js/popper.js"></script>
+	<script src="<?= media() ?>/tienda/vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= media() ?>/tienda/vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= media() ?>/tienda/vendor/daterangepicker/moment.min.js"></script>
+	<script src="<?= media() ?>/tienda/vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= media() ?>/tienda/vendor/slick/slick.min.js"></script>
+	<script src="<?= media() ?>/tienda/js/slick-custom.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= media() ?>/tienda/vendor/parallax100/parallax100.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= media() ?>/tienda/vendor/MagnificPopup/jquery.magnific-popup.min.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= media() ?>/tienda/vendor/isotope/isotope.pkgd.min.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= media() ?>/tienda/vendor/sweetalert/sweetalert.min.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= media() ?>/tienda/vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+<!--===============================================================================================-->
+	<script src="<?= media();?>/js/fontawesome.js"></script>
+	<script src="<?= media() ?>/tienda/js/main.js"></script>
+	<script src="<?= media();?>/js/functions_admin.js"></script>
+	<script src="<?= media() ?>/js/functions_login.js"></script>
+	<script src="<?= media() ?>/tienda/js/functions.js"></script>
+
+</body>
 </html>
