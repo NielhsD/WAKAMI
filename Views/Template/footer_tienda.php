66a7cc2536b591ff0f4a5c02a57a560f16d50590
ARIANNAEF22 <2101010123@undc.edu.pe>
2024-06-11 01:31:21 -0500

    Nuevo diseño del footer

--- Views/Template/footer_tienda.php
+++ Views/Template/footer_tienda.php
@@ -1,13 +1,57 @@
-	<?php 
+<?php 
 		$catFotter = getCatFooter();
 	 ?>
 	<!-- Footer -->
 	<footer class="bg3 p-t-75 p-b-32">
 		<div class="container">
+			<div class="row">
+				<div class="col-sm-6 col-lg-4 p-b-50">
+					<h4 class="stext-301 cl0 p-b-30">
+						Categorías
+					</h4>
+					<?php if(count($catFotter) > 0){ ?>
+					<ul>
+						<?php foreach ($catFotter as $cat) { ?>
+						<li class="p-b-10">
+							<a href="<?= base_url() ?>/tienda/categoria/<?= $cat['idcategoria'].'/'.$cat['ruta'] ?>" class="stext-107 cl7 hov-cl1 trans-04">
+								<?= $cat['nombre'] ?>
+							</a>
+						</li>
+						<?php } ?>
+					</ul>
+					<?php } ?>
+				</div>
 
-			 
+				<div class="col-sm-6 col-lg-4 p-b-50">
+					<h4 class="stext-301 cl0 p-b-30">
+						Contacto
+					</h4>
+
+					<p class="stext-107 cl7 size-201">
+						<?= DIRECCION ?> <br>
+						Celular: <a class="linkFooter" href="tel:<?= TELEMPRESA ?>"><?= TELEMPRESA ?></a><br>
+						Correo pedidos: <a class="linkFooter" href="mailto:<?= EMAIL_PEDIDOS ?>"><?= EMAIL_PEDIDOS ?></a><br>
+						Correo dueña: <a class="linkFooter" href="mailto:<?= EMAIL_EMPRESA ?>"><?= EMAIL_EMPRESA ?></a>
+					</p>
+
+					<div class="p-t-27">
+						<a href="<?= FACEBOOK ?>" target="_blank" class="fs-18 cl7 hov-cl1 trans-04 m-r-16">
+							<i class="fa fa-facebook"></i>
+						</a>
+
+						<a href="https://wa.me/<?= WHATSAPP ?>" target="_blank" class="fs-18 cl7 hov-cl1 trans-04 m-r-16">
+							<i class="fab fa-whatsapp"></i>
+						</a>
+					</div>
+				</div>
+
+				<div class="col-sm-12 col-lg-4 p-b-50 d-flex align-items-center">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d242.91223849702723!2d-76.35255016531241!3d-13.061379830337268!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x910ffbdd4c8cdadf%3A0x80e25b1cfd73cd6e!2sRicky%20Pan!5e0!3m2!1ses!2spe!4v1717949702216!5m2!1ses!2spe" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+				</div>
+			</div>
 		</div>
 	</footer>
+
 	<!-- Back to top -->
 	<div class="btn-back-to-top" id="myBtn">
 		<span class="symbol-btn-back-to-top">
@@ -50,12 +94,5 @@
 	<script src="<?= media() ?>/js/functions_login.js"></script>
 	<script src="<?= media() ?>/tienda/js/functions.js"></script>
 
-
-	<script>
-		function mostrarCotizacion(){
-			// alert()
-			$('#exampleModal').modal('show');
-		}
-	</script>
 </body>
 </html>
